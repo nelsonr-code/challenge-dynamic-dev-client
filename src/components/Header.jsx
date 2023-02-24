4240abdf0c6be02cb7cbba8426560701fa2c2db7
Nelson Rivas <nelson.rivas.ucv+code@gmail.com>
2023-02-24 08:36:18 -0400

    Redirect to home page from logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,14 @@
+import { Link } from 'react-router-dom'
+
 export const Header = () => {
   return (
     <header className='h-12 grid items-center bg-[#1b262a]'>
-      <div className='ml-2 font-medium uppercase text-[#9b9e9d]'>
-        Dynamic<span className='text-[#24bcca]'>Devs</span>
-        <span className='ml-2 font-normal lowercase'>Challenge</span>
-      </div>
+      <Link to='/'>
+        <div className='ml-2 font-medium uppercase text-[#9b9e9d]'>
+          Dynamic<span className='text-[#24bcca]'>Devs</span>
+          <span className='ml-2 font-normal lowercase'>Challenge</span>
+        </div>
+      </Link>
     </header>
   )
 }
