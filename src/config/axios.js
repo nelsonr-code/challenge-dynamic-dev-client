24a006160455c2434ee3b06f8935f084b57d308e
Nelson Rivas <nelson.rivas.ucv+code@gmail.com>
2023-02-27 09:16:10 -0400

    Changing base url for consuming api

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 export const webApi = axios.create({
-  // baseURL: 'http://localhost:3000/api',
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: 'https://musicfy-challenge.azurewebsites.net',
+  // baseURL: import.meta.env.VITE_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
